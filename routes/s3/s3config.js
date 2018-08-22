25fc00ba1b67c608ac338cd3de41ebbb95c11c2f
yoonkt200 <yoonkt200@gmail.com>
2018-08-22 15:32:24 +0900

    update

--- routes/s3/s3config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    region:'ap-northeast-2',
-    accessKeyId:'AKIAIGGRECIV5ZQ35N5Q',
-    secretAccessKey:'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',
-    bucketName:'fitbee-storage'
- };
